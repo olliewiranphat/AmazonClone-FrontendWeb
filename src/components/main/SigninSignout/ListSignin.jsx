7889705986b35740f37137bd14e8ea7a4de4ab40
olliewiranphat <oil.wiranphat@gmail.com>
2025-03-11 12:02:02 +0700

    Update Admin

--- src/components/main/SigninSignout/ListSignin.jsx
+++ src/components/main/SigninSignout/ListSignin.jsx
@@ -5,11 +5,11 @@ function ListSignin() {
 
     return (
         <SignedOut>
-            <ul className='absolute group-hover:block mt-[110px] left-[-42px] p-2 bg-white w-[150px] text-black'>
-                <SignInButton className='cursor-pointer bg-[#ffd814] rounded-md w-[130px] mx-auto flex justify-center items-center h-8' mode='modal'>
+            <ul className='hidden absolute group-hover:block top-12 rounded-md left-[-42px] p-2 bg-white w-[160px] text-black'>
+                <SignInButton className='cursor-pointer bg-[#ffd814] rounded-md w-[130px] my-2 mx-auto flex justify-center items-center h-8' mode='modal'>
                     Sign in
                 </SignInButton>
-                <div className='text-[9px] mt-2 pl-2'>
+                <div className='text-[9px] my-3 pl-2'>
                     <span className='mr-1'>New Customer?</span>
                     <SignUpButton mode='modal' className='cursor-pointer font-semibold underline text-blue-600'>Start here</SignUpButton>
                 </div>
--- src/store/UserStore.jsx
+++ src/store/UserStore.jsx
@@ -6,12 +6,12 @@ const authStore = (set) => ({ //set : set role as new value == Global State
     userData: null, //useZustand to keep State ROLE to show SELLER or ADMIN ??
     myOrder: null,
     selectedPDItem: [],
-    actionUserAccount: async (token, input) => {
+    actionUserAccount: async (token, input) => { //Update Account-ROLE
         const response = await createUpdateAccount(token, input)
         console.log('UserZustand', response);
         set({ userData: response.data.results })
     },
-    actionGetMyAccount: async (token) => {
+    actionGetMyAccount: async (token) => { //Get My Account
         const response = await getMyAccount(token)
         console.log('response', response);
         set({ userData: response.data.results })
